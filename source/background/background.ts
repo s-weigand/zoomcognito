1a3ec501a02ce3ad7538dd305e90bb1f09c5c8bf
s-weigand <s.weigand.phy@gmail.com>
2020-04-26 22:35:35 +0200

    adjusted context menu to fit the project

--- source/background/background.ts
+++ source/background/background.ts
@@ -1,2 +1,21 @@
-// tslint-disable-next-line import/no-unassigned-import
-import '../options/options-storage'
+import { browser } from 'webextension-polyfill-ts'
+
+import { blockZoomClientDownload } from '../lib/browser_functions'
+import optionsStorage from '../options/options-storage'
+
+optionsStorage
+  .getAll()
+  .then((response) => {
+    blockZoomClientDownload(response.blockClientDownload)
+  })
+  .catch(() => {
+    alert('Error blocking Zoom client download.')
+  })
+
+const messageListener = (data: any) => {
+  if (data.triggerBlockClientDownloadReload !== undefined) {
+    blockZoomClientDownload(data.triggerBlockClientDownloadReload)
+  }
+}
+
+browser.runtime.onMessage.addListener(messageListener)
--- source/context_menue/context_menue_entry.ts
+++ source/context_menue/context_menue_entry.ts
@@ -1,53 +1,42 @@
 import { Menus, Tabs, browser } from 'webextension-polyfill-ts'
 
-import { openInIncognitoWindow } from '../lib/functions'
-
-export const contextMenueContainerId = 'awesome-extension-context-container'
-
-browser.contextMenus.create({
-  id: contextMenueContainerId,
-  title: 'Extension template context menue',
-  contexts: ['all'],
-})
+import { openInIncognitoWindow } from '../lib/browser_functions'
+import { generateZoomWebLink, isZoomMeetingUrl } from '../lib/parsers'
+import optionsStorage from '../options/options-storage'
 
 browser.contextMenus.create({
-  id: 'awesome-extension-context-incognito',
-  parentId: contextMenueContainerId,
-  title: 'Open url in incognito windows',
-  contexts: ['all'],
-})
-
-browser.contextMenus.create({
-  id: 'awesome-extension-context-sidebar',
-  parentId: contextMenueContainerId,
-  title: 'Toggle sidebar',
-  contexts: ['all'],
+  id: 'zoomcognito-context-open-incognito',
+  title: 'Open zoom in incognito windows',
+  contexts: ['link'],
 })
 
 // not working in firefox
-const currentUrlIcognito = (_info: Menus.OnClickData, tab?: Tabs.Tab | undefined) => {
-  if (tab !== undefined) {
-    const currentUrl = tab.url
-    if (currentUrl !== undefined) {
-      openInIncognitoWindow(currentUrl)
-      return
+const currentUrlIcognito = (info: Menus.OnClickData, _tab?: Tabs.Tab | undefined) => {
+  const linkUrl = info.linkUrl
+  if (linkUrl !== undefined) {
+    if (isZoomMeetingUrl(linkUrl) === true) {
+      optionsStorage
+        .getAll()
+        .then((response) => {
+          const urlPrefix = response.zoomUrlPrefix
+          const zoomWebLink = generateZoomWebLink(linkUrl, urlPrefix)
+          openInIncognitoWindow(zoomWebLink)
+        })
+        .catch(() => {
+          alert('Error getting the value of the option zoomUrlPrefix.')
+        })
+    } else {
+      alert(`The given url isn't a proper zoom meeting url:\n ${linkUrl}`)
     }
+  } else {
+    alert("Couldn't determine url.")
   }
-  alert("Couldn't determine url.")
 }
 
-// not working in chrome
-const toggleAwesomeSidebar = (_info: Menus.OnClickData, _tab?: Tabs.Tab | undefined) => {
-  browser.sidebarAction.toggle()
-}
-
-const triggerContextAction = (_info: Menus.OnClickData, tab?: Tabs.Tab | undefined) => {
-  switch (_info.menuItemId) {
-    case 'awesome-extension-context-incognito':
-      currentUrlIcognito(_info, tab)
-      break
-    case 'awesome-extension-context-sidebar':
-      toggleAwesomeSidebar(_info, tab)
+const triggerContextAction = (info: Menus.OnClickData, tab?: Tabs.Tab | undefined) => {
+  switch (info.menuItemId) {
+    case 'zoomcognito-context-open-incognito':
+      currentUrlIcognito(info, tab)
       break
   }
 }
