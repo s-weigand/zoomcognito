4295e85c91ac570ce68aa6e45c5387f360e2c286
s-weigand <s.weigand.phy@gmail.com>
2020-04-26 22:31:59 +0200

    added jest tests for the parsers

---
+++ tests/lib/parsers.test.ts
@@ -0,0 +1,100 @@
+import { generateZoomWebLink, isZoomMeetingUrl } from '../../source/lib/parsers'
+
+describe('testing lib/parsers', () => {
+  describe('isZoomUrl', () => {
+    it.each([
+      'https://zoom.us/wc/join/12345',
+      'https://zoom.us/wc/join/12345?pwd=password123',
+      'http://zoom.us/wc/join/12345?pwd=password123',
+      'https://zoom.us/join/12345?pwd=password123',
+      'https://zoom.us/j/12345?pwd=password123',
+      'https://zoom.us/12345/j?pwd=password123',
+      'https://zoom.us/start/12345?pwd=password123',
+      'https://zoom.us/s/12345?pwd=password123',
+      'https://sub-domain.zoom.us/wc/join/12345?pwd=password123',
+      'https://zoom.us/wc/join/12345/join?pwd=password123', // even so this is technically invalid, it allow generation of a valid url
+    ])('Valid url: %s', (validUrl) => {
+      const result = isZoomMeetingUrl(validUrl)
+      expect(result).toBe(true)
+    })
+    it.each([
+      'https://google.com',
+      'https://zoom.us',
+      'https://zoom.us/12345',
+      'https://zoom.us/j',
+      'https://zoom.us/wc/j',
+    ])('Invalid url: %s', (invalidUrl) => {
+      const result = isZoomMeetingUrl(invalidUrl)
+      expect(result).toBe(false)
+    })
+  })
+
+  describe('generateZoomWebLink', () => {
+    it.each([
+      [
+        'https://zoom.us/wc/join/12345',
+        'dummy-domain',
+        'https://dummy-domain.zoom.us/wc/12345/join',
+      ],
+      ['https://zoom.us/wc/join/12345', '', 'https://zoom.us/wc/12345/join'],
+      [
+        'https://zoom.us/wc/join/12345?pwd=password123',
+        '',
+        'https://zoom.us/wc/12345/join?pwd=password123',
+      ],
+      [
+        'http://zoom.us/wc/join/12345?pwd=password123',
+        '',
+        'https://zoom.us/wc/12345/join?pwd=password123',
+      ],
+      [
+        'https://zoom.us/join/12345?pwd=password123',
+        '',
+        'https://zoom.us/wc/12345/join?pwd=password123',
+      ],
+      [
+        'https://zoom.us/j/12345?pwd=password123',
+        '',
+        'https://zoom.us/wc/12345/join?pwd=password123',
+      ],
+      [
+        'https://zoom.us/12345/j?pwd=password123',
+        '',
+        'https://zoom.us/wc/12345/join?pwd=password123',
+      ],
+      [
+        'https://zoom.us/start/12345?pwd=password123',
+        '',
+        'https://zoom.us/wc/12345/start?pwd=password123',
+      ],
+      [
+        'https://zoom.us/s/12345?pwd=password123',
+        '',
+        'https://zoom.us/wc/12345/start?pwd=password123',
+      ],
+      [
+        'https://sub-domain.zoom.us/wc/join/12345?pwd=password123',
+        '',
+        'https://sub-domain.zoom.us/wc/12345/join?pwd=password123',
+      ],
+      [
+        'https://zoom.us/wc/join/12345/join?pwd=password123',
+        '',
+        'https://zoom.us/wc/12345/join?pwd=password123',
+      ], // even so this is technically invalid, it allow generation of a valid url
+    ])('Valid url: %s', (validUrl, urlPrefix, zoomWebLink) => {
+      const result = generateZoomWebLink(validUrl, urlPrefix)
+      expect(result).toBe(zoomWebLink)
+    })
+    it.each([
+      'https://google.com',
+      'https://zoom.us',
+      'https://zoom.us/12345',
+      'https://zoom.us/j',
+      'https://zoom.us/wc/j',
+    ])('Invalid url: %s', (invalidUrl) => {
+      const result = generateZoomWebLink(invalidUrl)
+      expect(result).toBe('')
+    })
+  })
+})
