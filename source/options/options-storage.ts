72c2d951edfb406b2d7e423b1124a1b7abbae31c
s-weigand <s.weigand.phy@gmail.com>
2020-04-27 08:53:55 +0200

    changed default username to zoomCognitoUser

--- source/options/options-storage.ts
+++ source/options/options-storage.ts
@@ -2,7 +2,7 @@ import OptionsSync from 'webext-options-sync'
 
 export default new OptionsSync({
   defaults: {
-    username: 'zoom_user',
+    username: 'zoomCognitoUser',
     autoAcceptUserAgreement: false,
     zoomUrlPrefix: '',
     blockClientDownload: true,
